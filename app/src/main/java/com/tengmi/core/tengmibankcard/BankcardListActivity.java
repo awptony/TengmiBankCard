0c9b13d63687027b8baca1df448b0e5bbbe6d6b9
qhw <2542121970@qq.com>
2018-07-02 14:01:15 +0800

    first commit

---
+++ app/src/main/java/com/tengmi/core/tengmibankcard/BankcardListActivity.java
@@ -0,0 +1,17 @@
+package com.tengmi.core.tengmibankcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2018/7/2 0002.
+ */
+
+public class BankcardListActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
